a53b3a11a8432ac88b30d7a34899611e5b812d7a
Matthias Urlichs <matthias@urlichs.de>
2022-03-16 13:43:23 +0100

    Start switching to async+httpx

--- sample/zoom.py
+++ sample/zoom.py
@@ -3,6 +3,7 @@ import json
 import jwt
 import config
 import time
+import trio
 from pprint import pprint
 
 with open("data/zoom/phone.json","r") as _f:
@@ -17,16 +18,24 @@ del _s
 def generate_jwt(key, secret):
     header = {"alg": "HS256", "typ": "JWT"}
 
-    payload = {"iss": key, "exp": int(time.time() + 3600)}
+    payload = {"iss": key, "exp": int(time.time() + 3600*24*365*10), }
+    #payload = {"appKey": key, "iat": int(time.time()), "exp": int(time.time() + 3600), "tokenExp":3600}
 
     token = jwt.encode(payload, secret, algorithm="HS256", headers=header)
-    return token.decode("utf-8")
+    return token.decode("ascii")
 
-token = generate_jwt(config.API_KEY, config.API_SECRET)
-breakpoint()
-api.authenticate('Bearer', "Bearer "+token)
-pprint(api.call_listAutoReceptionists())
+async def main():
+    token = generate_jwt(config.API_KEY, config.API_SECRET)
+    print(token)
+    api.authenticate('Bearer', "Bearer "+token)
+#pprint(api.call_listAutoReceptionists())
+    res=await api.call_listAccountPhoneNumbers()
+    pprint(res)
+    breakpoint()
 
+#import requests
+#r = requests.get("https://api.zoom.us/v2/phone/blocked_list", headers={'Authorization': 'Bearer '+token})
+#print(r,r.text)
 
-pass
+trio.run(main)
 
