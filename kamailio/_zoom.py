d8f0e9bba7e7a827ffa6275cf77b21c3a894141c
Matthias Urlichs <smurf@noris.de>
2022-09-30 09:41:45 +0200

    Move data dir

--- kamailio/_zoom.py
+++ kamailio/_zoom.py
@@ -77,7 +77,7 @@ async def refresh_auth(api, task_status=trio.TASK_STATUS_IGNORED):
         await trio.sleep(1700)
 
 async def main(setup_done=lambda: None):
-    with open("/root/kamailio-py/data/zoom/phone.json","r") as _f:
+    with open(os.path.join(os.pardir(__path__[0]),"_data","zoom","phone.json"),"r") as _f:
         _s = json.load(_f)
 
     async with OpenAPI(_s) as api, trio.open_nursery() as n:
