be2931d04c12db4b68a8ae8b7cd1006766e304b3
Matthias Urlichs <matthias@urlichs.de>
2024-11-20 20:31:23 +0100

    update to current ._zoom

--- kamailio/zoom.py
+++ kamailio/zoom.py
@@ -8,10 +8,18 @@ import math
 
 import trio
 
+import logging
+logger = logging.getLogger(__name__)
 
 async def init(kam):
-    kam  ## noqa:B018
-    from ._zoom import zoom_worker
+    from ._zoom import ZoomWrapper
 
-    async with zoom_worker():
+    async with (
+            ZoomWrapper(kam.cfg, _debug=True) as z,
+            trio.open_nursery() as n,
+            ):
+        await n.start(z.refresh_token)
+        await n.start(z.refresh_numbers)
+        await n.start(z.app_server)
+        logger.debug("Zoom subsystem is up.")
         await trio.sleep(math.inf)
