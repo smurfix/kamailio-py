62e94563fb80a56303827e02750fea1677fb09d1
Matthias Urlichs <matthias@urlichs.de>
2024-11-20 20:28:41 +0100

    Set up sensible logging

--- kamailio/__init__.py
+++ kamailio/__init__.py
@@ -2,6 +2,9 @@
 from __future__ import annotations
 
 import threading
+import logging
+import logging.config
+
 from sys import exit  # noqa:F401
 
 from . import log as log  # noqa:PLC0414  # ruff bug *sigh*
@@ -10,6 +13,31 @@ try:  # noqa:SIM105
     from . import var  # noqa:F401
 except ImportError:
     pass
+
+LOGGING = {
+    'version': 1,
+    'disable_existing_loggers': False,
+    'formatters': {
+        'verbose': {
+            'format': '%(levelname)s %(module)s P%(process)d T%(thread)d %(message)s'
+            },
+        },
+    'handlers': {
+        'syslog': {
+            'class': 'logging.handlers.SysLogHandler',
+            'address': '/dev/log',
+            'facility': "local0",
+            'formatter': 'verbose',
+            },
+        },
+    'root': {
+        'handlers': ['syslog'],
+        'level': logging.DEBUG,
+        'propagate': True,
+        },
+    }
+
+logging.config.dictConfig(LOGGING)
 
 
 class _State(threading.local):
